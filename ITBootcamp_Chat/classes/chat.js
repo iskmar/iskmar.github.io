010a789b227b135317cd0904baaccebaee071ab3
iskmar <iskrenovicmarko@gmail.com>
2020-09-25 12:58:01 +0200

    some changes

---
+++ ITBootcamp_Chat/classes/chat.js
@@ -0,0 +1,65 @@
+export class Chatroom {
+	constructor (room,username) {
+		this.room = room;
+		this.username = username;
+		this.chats = db.collection('chats');
+		this.unsub;
+	}
+
+	set room(r) {
+		this._room = r;
+	}
+	set username (u) {
+		this._username = u;
+	}
+
+	get room() {
+		return this._room;
+	}
+	get username() {
+		return this._username;
+	}
+	updateUsername(updateUsername) {
+		if(updateUsername.length >= 2 && updateUsername.length <= 10) {
+			this.username = updateUsername;
+			localStorage.setItem('name', updateUsername.toLowerCase());
+		} else {
+			alert('Ime mora biti najmanje 2 karaktera a najvise 10!');
+		}
+	}
+	updateRoom(updateRoom) {
+		this.room = updateRoom;
+		if(this.unsub) {
+			this.unsub();
+		}
+	}
+	async addChat(msg) {
+		let chat = {
+			message: msg,
+			username: this.username,
+			room: this.room,
+			created_at: firebase.firestore.Timestamp.fromDate(new Date())
+		}
+		let response = await this.chats.add(chat);
+		return response;
+
+	}
+	async getChats ( cb ) {
+		this.unsub = this.chats
+			.where( 'room', '==', this.room )
+			.orderBy( 'created_at', 'asc' )
+			.onSnapshot( snapshot => {
+				snapshot.docChanges().forEach( change => {
+					let type = change.type;
+					let doc = change.doc;
+					if ( type === 'added' ) {
+						let obj = doc.data();
+						cb( obj );
+
+					}
+				} )
+			} )
+	}
+
+
+}
---
+++ ITBootcamp_Chat/modules/functions.js
@@ -0,0 +1,26 @@
+
+let checkName = () => {
+	let name = localStorage.getItem('name');
+	if(!name) {
+		return 'guest';
+	} else {
+		return name;
+	}
+}
+let getRoomId = (buttons) => {
+	for ( let i = 0; i < buttons.length; i++ ) {
+		buttons[ i ].addEventListener( 'click', event => {
+			event.preventDefault();
+			return buttons[i].id;
+		} )
+	}
+}
+let getMessages = (obj,obj1) => {
+	obj.getChats(data => {
+		obj1.templateUI(data);
+	})
+}
+let loadChat = () => {
+	document.getElementById('general').click();
+}
+export { getMessages, getRoomId , checkName, loadChat};
